50129591226e1c83ba2aa7b5d66ee56f48b5c80b
uKaxg <uka.pgr@gmail.com>
2023-10-07 12:54:15 +0700

    add prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 function App() {
-  return (
-    <>
-      <div>Code is going here</div>
-    </>
-  )
+   return (
+      <>
+         <div>Code is going here</div>
+      </>
+   );
 }
 
-export default App
+export default App;
