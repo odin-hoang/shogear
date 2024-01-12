95f5ffd557bf752f65f1dcd591e629d864475cfe
uKaxg <uka.pgr@gmail.com>
2024-01-12 12:13:50 +0700

    Refactor OrderResultItem component and update props

--- src/layouts/components/order-results/OrderResultItem.tsx
+++ src/layouts/components/order-results/OrderResultItem.tsx
@@ -1,35 +1,26 @@
-import { useEffect, useState } from 'react';
 import { numberWithCommas } from '../../../lib/scripts';
-import { getPostDetail } from '../../../services/productService';
+import { ProductItem } from '../../../pages/ProductDetail';
 
-const OrderResultItem = ({ product }: { product: any }) => {
-    const [item, setItem] = useState<any>();
-    useEffect(() => {
-        const fetch = async () => {
-            const data = await getPostDetail(product?.product);
-            console.log();
-            setItem(data);
-        };
-        if (product?.product) fetch();
-    }, []);
+const OrderResultItem = ({ item }: { item: any }) => {
+    const product = item.product as ProductItem;
     return (
         <>
             <tr>
                 <td className='min-w-[350px] max-w-[450px]'>
                     <div className='item?s-center flex pr-8'>
                         <figure className='ml-4 mr-[1.8rem] max-w-[60px]'>
-                            <img src={item?.imageUrl} alt='' />
+                            <img src={product?.attachments[0].file} alt='' />
                         </figure>
-                        <h3 className='overflow-hidden'>{item?.name}</h3>
+                        <h3 className='overflow-hidden'>{product?.name}</h3>
                     </div>
                 </td>
-                <td className='min-w-[140px]'>{numberWithCommas(item?.price)} VND</td>
+                <td className='min-w-[140px]'>{numberWithCommas(product?.price)} VND</td>
                 <td className='min-w-[135px]'>
                     <div className='w-[100px]'>
                         <div className='position-relative item?s-center flex h-[40px] w-[100%] border border-solid border-[#999]'>
                             <input
                                 type='text'
-                                value={product?.quantity}
+                                value={item.quantity}
                                 className='position-absolute w-full text-center outline-none'
                                 min={1}
                                 max={9999}
@@ -37,9 +28,7 @@ const OrderResultItem = ({ product }: { product: any }) => {
                         </div>
                     </div>
                 </td>
-                <td className='min-w-[160px] text-[#fcb941]'>
-                    {numberWithCommas(item?.price * product?.quantity)} VND
-                </td>
+                <td className='min-w-[160px] text-[#fcb941]'>{numberWithCommas(product?.price * item.quantity)} VND</td>
             </tr>
         </>
     );
